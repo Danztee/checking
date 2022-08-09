6f738aabd01f04342185bf8911674bfb6671950e
basitol <basitolaitan27@gmail.com>
2022-08-09 22:12:57 +0100

    build: dummy data

---
+++ src/data.js
@@ -0,0 +1,64 @@
+import {
+  Airpods,
+  Iphone,
+  Laptop,
+  Suit,
+  Sneakers,
+  Sneakers2,
+  Sneakers3,
+} from "./assets";
+
+export const slideData = [
+  {
+    id: 1,
+    category: "Accessories",
+    img: Iphone,
+  },
+  {
+    id: 2,
+    category: "Accessories",
+    img: Airpods,
+  },
+  {
+    id: 3,
+    category: "Accessories",
+    img: Laptop,
+  },
+  {
+    id: 4,
+    category: "Gents",
+    img: Suit,
+  },
+  {
+    id: 5,
+    category: "Gents",
+    img: Suit,
+  },
+];
+
+export const productData = [
+  {
+    id: 1,
+    name: "lorem ipsum",
+    price: 6000,
+    rating: 4.5,
+    img: Sneakers,
+    favorite: false,
+  },
+  {
+    id: 2,
+    name: "lorem ipsum",
+    price: 6000,
+    rating: 4.5,
+    img: Sneakers2,
+    favorite: false,
+  },
+  {
+    id: 3,
+    name: "lorem ipsum",
+    price: 6000,
+    rating: 4.5,
+    img: Sneakers3,
+    favorite: false,
+  },
+];
