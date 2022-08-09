0fd80129e4668a85625f27657d37edb798f92acd
basitol <basitolaitan27@gmail.com>
2022-08-09 22:20:11 +0100

    build: export for component

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Footer } from "./Footer/Footer";
+export { default as ProfileCard } from "./Profile/ProfileCard";
+export { default as ProductCard } from "./Product/ProductCard";
