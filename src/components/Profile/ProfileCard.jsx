5753ce46e4126aaa13083afc962f22acd5be5b67
basitol <basitolaitan27@gmail.com>
2022-08-11 23:59:44 +0100

    add: upload image to profile card

--- src/components/Profile/ProfileCard.jsx
+++ src/components/Profile/ProfileCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { FaRegUser } from "react-icons/fa";
 import { MdFavoriteBorder } from "react-icons/md";
 import { GiBackwardTime } from "react-icons/gi";
-import { FiSettings } from "react-icons/fi";
+import { FiSettings, FiCamera } from "react-icons/fi";
 import { HiOutlineLogout } from "react-icons/hi";
 import { MdKeyboardArrowUp } from "react-icons/md";
 import "animate.css";
@@ -13,16 +13,22 @@ import "./profile.scss";
 const ProfileCard = ({ closeHandler, className }) => {
   return (
     <div
-      className={`${className} animate__animated animate__fadeIn profile-card absolute top-20 right-10`}
+      className={`${className} animate__animated animate__fadeIn profile-card absolute top-8 -right-10`}
     >
       <div className="profile-card-info flex justify-between">
         <div className="flex">
-          <div className="profile-card-info-img w-12 h-12 mr-3">
+          <div className="profile-card-info-img w-12 h-12 mr-3 relative">
             <img
               src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
               alt=""
               className="w-full h-full"
             />
+            <div className="profile-card-info-img-upload absolute bottom-0 right-0">
+              <input type="file" id="profilePhotoInput" name="photo" />
+              <label htmlFor="profilePhotoInput" className="">
+                <FiCamera />
+              </label>
+            </div>
           </div>
           <div className="profile-card-info-name">
             <h1>John Doe</h1>
