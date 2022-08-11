bfe405945d78d130207edf11acfadab236f0d463
basitol <basitolaitan27@gmail.com>
2022-08-11 23:59:09 +0100

    build: export for carousel

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar/Navbar";
 export { default as Footer } from "./Footer/Footer";
 export { default as ProfileCard } from "./Profile/ProfileCard";
 export { default as ProductCard } from "./Product/ProductCard";
+export { default as Carousel } from "./Carousel/Carousel";
