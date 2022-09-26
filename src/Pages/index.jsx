7264df354c6ebf2bde06486e68499daf78f91fa0
basitol <basitolaitan27@gmail.com>
2022-09-27 00:44:02 +0100

    build: .....

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -2,3 +2,5 @@ export { default as Home } from "./Home/Home";
 export { default as SignIn } from "./User/SignIn";
 export { default as Auth } from "./User/Auth";
 export { default as ProductPage } from "./Product/ProductPage";
+export { default as ProductCategory } from "./Product/ProductCategory";
+export { default as ProductDetails } from "./Product/ProductDetails";
