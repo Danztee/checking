3c5469102b5db7bc88d1a7a6af878966f6697341
basitol <basitolaitan27@gmail.com>
2022-09-27 00:40:11 +0100

    build: store for states

--- src/store.js
+++ src/store.js
@@ -1,17 +1,35 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReducer } from "./reducers/productReducer";
+import {
+  productListReducer,
+  productCategoryReducer,
+  productDetailReducer,
+} from "./reducers/productReducer";
+import { userRegisterReducer, userLoginReducer } from "./reducers/userReducer";
 
 const reducer = combineReducers({
   // Add your reducers here
   productList: productListReducer,
+  productCategory: productCategoryReducer,
+  userRegister: userRegisterReducer,
+  userLogin: userLoginReducer,
+  productDetail: productDetailReducer,
 });
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : [];
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
 const middleware = [thunk];
 
 const store = createStore(
   reducer,
+  initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
