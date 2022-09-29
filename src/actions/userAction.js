b255b8423565c7061ced006b1ae6e5d5e8ff8bf1
basitol <basitolaitan27@gmail.com>
2022-09-29 20:42:21 +0100

    build: user details  and user profile action

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -7,6 +7,12 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_UPDATE_PROFILE_FROM_STORAGE_REQUEST,
+  USER_UPDATE_PROFILE_FROM_STORAGE_SUCCESS,
+  USER_UPDATE_PROFILE_FROM_STORAGE_FAIL,
 } from "../constants/userConstant";
 
 export const login = (email, password) => async (dispatch) => {
@@ -81,5 +87,67 @@ export const register = (name, email, password) => async (dispatch) => {
           : error.error,
     });
     console.log(error);
+  }
+};
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(
+      `https://wicartit.herokuapp.com/api/users/${id}`,
+      config
+    );
+
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const getUserProfileFromLocalStorage = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_UPDATE_PROFILE_FROM_STORAGE_REQUEST,
+    });
+
+    const userInfo = localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo"))
+      : null;
+
+    dispatch({
+      type: USER_UPDATE_PROFILE_FROM_STORAGE_SUCCESS,
+      payload: userInfo,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_PROFILE_FROM_STORAGE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
