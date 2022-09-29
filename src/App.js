505e9a2a112524f5d2d55194607f89116520b311
basitol <basitolaitan27@gmail.com>
2022-09-29 20:40:18 +0100

    build: route for  profile page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   ProductPage,
   ProductCategory,
   ProductDetails,
+  Profile,
 } from "./Pages";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
@@ -24,6 +25,7 @@ function App() {
           {["/login", "/register"].map((path) => (
             <Route path={path} element={<Auth />} key={path} />
           ))}
+          <Route path="/profile" element={<Profile />} />
           <Route path="/products" element={<ProductPage />} />
           <Route path="/products/:category" element={<ProductCategory />} />
           <Route path="/product/:productId" element={<ProductDetails />} />
