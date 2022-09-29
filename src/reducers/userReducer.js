f1c2dee7f7370248f945761a336f3b3f29e5f743
basitol <basitolaitan27@gmail.com>
2022-09-29 20:41:59 +0100

    build: user details  and user profile reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -6,6 +6,12 @@ import {
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_UPDATE_PROFILE_FROM_STORAGE_FAIL,
+  USER_UPDATE_PROFILE_FROM_STORAGE_REQUEST,
+  USER_UPDATE_PROFILE_FROM_STORAGE_SUCCESS,
 } from "../constants/userConstant";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -36,6 +42,53 @@ export const userRegisterReducer = (state = {}, action) => {
         success: true,
       };
     case USER_REGISTER_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case USER_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        user: action.payload,
+      };
+    case USER_DETAILS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const userProfileFromLocalStorageReducer = (
+  state = { user: {} },
+  action
+) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_FROM_STORAGE_REQUEST:
+      return {
+        loading: true,
+      };
+    case USER_UPDATE_PROFILE_FROM_STORAGE_SUCCESS:
+      return {
+        loading: false,
+        user: action.payload,
+        success: true,
+      };
+    case USER_UPDATE_PROFILE_FROM_STORAGE_FAIL:
       return {
         loading: false,
         error: action.payload,
